6ea05f5222e06fb2d6a926932449ba7c505b3ab7
su <1571104304@qq.com>
2018-05-09 14:19:24 +0800

    redis存储变为hash

--- src/main/java/com/share/service/impl/ManagerRedisServiceImpl.java
+++ src/main/java/com/share/service/impl/ManagerRedisServiceImpl.java
@@ -1,8 +1,10 @@
 package com.share.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.share.pojo.Manager;
 import com.share.service.IManagerRedisService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
@@ -19,17 +21,20 @@ public class ManagerRedisServiceImpl implements IManagerRedisService {
     @Autowired
     private RedisTemplate redisTemplate;
     Collection<String> colls = new ArrayList<String>();
+
     @Override
     public void addManager(Manager manager) {
-        ValueOperations<String, Manager> valueops = redisTemplate.opsForValue();
-        valueops.set(manager.getMname(), manager);
-        colls.add(manager.getMname());
+        //todo 改用hash类型
+        HashOperations<String, String, String> hashOperations = redisTemplate.opsForHash();
+        hashOperations.put("manager", manager.getMname(), JSON.toJSONString(manager));
     }
 
     @Override
     public Manager getManger(String username) {
-        ValueOperations<String, Manager> valueOperations = redisTemplate.opsForValue();
-        Manager manager = valueOperations.get(username);
+        //todo hash
+        HashOperations<String, String, String> hashOperations = redisTemplate.opsForHash();
+        String res = hashOperations.get("manager", username);
+        Manager manager=JSON.parseObject(res,Manager.class);
         return manager;
     }
 }
