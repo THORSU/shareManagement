28c46ef6fbe3dbce639f031e8dd945762a6b6014
su <1571104304@qq.com>
2018-05-19 23:16:12 +0800

    修改登录逻辑

--- src/main/resources/mapper/mapper.sql
+++ src/main/resources/mapper/mapper.sql
@@ -1,56 +1,19 @@
-# 管理员表
-DROP TABLE IF EXISTS Manager;
-CREATE TABLE Manager
-(
-  mid       INT (11)    NOT NULL AUTO_INCREMENT
-    PRIMARY KEY,
-  mname     VARCHAR(20) NOT NULL,
-  mpassword VARCHAR(20) NULL
+DROP TABLE IF EXISTS `User`;
+CREATE TABLE `User` (
+  `uid`       INT(11)       NOT NULL AUTO_INCREMENT,
+  `uname`     VARCHAR(255)  NOT NULL,
+  `upwd`      VARCHAR(255)  NOT NULL,
+  `alias`     VARCHAR(16)   NULL,
+  `umobile`   VARCHAR(20)   NULL,
+  `IDnumber`  VARCHAR(20)   NULL,
+  `wallet`    DOUBLE(10, 1) NOT NULL,
+  `condition` VARCHAR(10)   NOT NULL,
+  PRIMARY KEY (`uid`)
 )
   ENGINE = InnoDB
-  AUTO_INCREMENT=1
-  COLLATE = utf8_unicode_ci
-  COMMENT='管理员';
-#商品表
-DROP TABLE IF EXISTS bikeTable;
-CREATE TABLE bikeTable
-(
-  id          INT(20)      NOT NULL
-  COMMENT '主键id'
-    PRIMARY KEY,
-  objectId    INT(20)      NULL
-  COMMENT '商品主表对应id',
-  code        VARCHAR(20)  NULL
-  COMMENT '子商品序列号',
-  password    VARCHAR(20)  NULL
-  COMMENT '子商品密码',
-  `condition` TINYINT(1)   NULL
-  COMMENT '是否可用（好坏）',
-  remark      VARCHAR(200) NULL
-  COMMENT '商品备注'
-)
-  ENGINE = InnoDB
-  CHARSET = utf8;
+  AUTO_INCREMENT = 1000
+  DEFAULT CHARSET = utf8;
 
-# 商品表
-DROP TABLE IF EXISTS Objects;
-CREATE TABLE Objects
-(
-  id     INT(20) AUTO_INCREMENT
-  COMMENT '主键id'
-    PRIMARY KEY,
-  code   VARCHAR(20)  NULL
-  COMMENT '商品序列号',
-  name   VARCHAR(50)  NULL
-  COMMENT '商品名称',
-  price  DOUBLE(5, 2) NULL
-  COMMENT '商品租用价格',
-  remark VARCHAR(200) NULL
-  COMMENT '商品备注'
-)
-  ENGINE = InnoDB
-  CHARSET = utf8;
-#订单表
 DROP TABLE IF EXISTS Orders;
 CREATE TABLE Orders
 (
@@ -63,7 +26,7 @@ CREATE TABLE Orders
   COMMENT '商品主表对应id',
   `	subObjectId` INT(20)     NULL
   COMMENT '商品子表id',
-  code            VARCHAR(20) NULL
+  order_code      VARCHAR(20) NULL
   COMMENT '订单号',
   bill            DECIMAL(2)  NULL
   COMMENT '消费金额',
@@ -76,19 +39,41 @@ CREATE TABLE Orders
 )
   ENGINE = InnoDB
   CHARSET = utf8;
-#用户表
-DROP TABLE IF EXISTS User;
-CREATE TABLE User
+
+DROP TABLE IF EXISTS Objects;
+CREATE TABLE Objects
 (
-  uid         INT AUTO_INCREMENT
+  id          INT(20) AUTO_INCREMENT
+  COMMENT '主键id'
     PRIMARY KEY,
-  uname       VARCHAR(255)  NOT NULL,
-  upwd        VARCHAR(255)  NOT NULL,
-  alias       VARCHAR(16)   NULL,
-  umobile     VARCHAR(20)   NULL,
-  IDnumber    VARCHAR(20)   NULL,
-  wallet      DOUBLE(10, 1) NOT NULL,
-  `condition` VARCHAR(10)   NOT NULL
+  Object_code VARCHAR(20)  NULL
+  COMMENT '商品序列号',
+  name        VARCHAR(50)  NULL
+  COMMENT '商品名称',
+  price       DOUBLE(5, 2) NULL
+  COMMENT '商品租用价格',
+  remark      VARCHAR(200) NULL
+  COMMENT '商品备注'
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 1000
+  CHARSET = utf8;
+DROP TABLE IF EXISTS bikeTable;
+CREATE TABLE bikeTable
+(
+  id          INT(20)      NOT NULL
+  COMMENT '主键id'
+    PRIMARY KEY,
+  objectId    INT(20)      NULL
+  COMMENT '商品主表对应id',
+  sub_code    VARCHAR(20)  NULL
+  COMMENT '子商品序列号',
+  password    VARCHAR(20)  NULL
+  COMMENT '子商品密码',
+  `condition` TINYINT(1)   NULL
+  COMMENT '是否可用（好坏）',
+  remark      VARCHAR(200) NULL
+  COMMENT '商品备注'
 )
   ENGINE = InnoDB
   CHARSET = utf8;
